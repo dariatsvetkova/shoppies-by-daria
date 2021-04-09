6c409035d14bf8d68b1f18df80af41676aab9175
dariatsvetkova <d.s.tsvetkova@hotmail.com>
2021-04-08 20:22:07 -0400

    convert to functional component

--- src/Components/MovieCard/MovieCard.js
+++ src/Components/MovieCard/MovieCard.js
@@ -1,100 +1,94 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Placeholder from '../../Images/placeholder.png';
 import MovieInfo from '../MovieInfo/MovieInfo';
 
 import '../global.css';
 import * as styles from './movieCard.module.css';
 
-class MovieCard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      viewingInfo: false,
-    };
-    this.showInfo = this.showInfo.bind(this);
-  }
+const MovieCard = ({
+  movie,
+  category,
+  disableNominate,
+  remove,
+}) => {
+  const [showInfo, setShowInfo] = useState(false);
 
   // Open/close the movie info card
-  showInfo() {
-    if (this.state.viewingInfo) {
+  const openInfo = () => {
+    if (showInfo) {
       // If the card is closing, trigger the animation:
       const closedInfo = document.querySelector('.movieInfo');
       closedInfo.classList.add('disappear');
 
-      setTimeout(() => this.setState({viewingInfo: false}),
+      setTimeout(() => setShowInfo(false),
           300);
     } else {
       // Show the card:
-      this.setState({viewingInfo: true});
+      setShowInfo(true);
     }
-  }
+  };
 
-  render() {
-    const {
-      imdbID, Title, Year, Poster,
-    } = this.props.movie;
-    const classCategory = `movieCard${this.props.category[0].toUpperCase()}${this.props.category.slice(1)}`;
+  const {
+    imdbID, Title, Year, Poster,
+  } = movie;
+  const classCategory = `movieCard${category[0].toUpperCase()}${category.slice(1)}`;
 
-    return (
-      <li>
-        <article
-          className={`${styles.movieCard} ${styles[classCategory]} box`}
-          id={imdbID}
-        >
-          <div>
-            <img
-              src={Poster.match(/^http|^www/) ? Poster : Placeholder}
-              alt={`${Title} movie poster`}
-            />
-            <h4 className={styles.movieTitle}>
-              {Title}
-              {' '}
-              (
-              {Year}
-              )
-            </h4>
-          </div>
+  return (
+    <li>
+      <article
+        className={`${styles.movieCard} ${styles[classCategory]} box`}
+        id={imdbID}
+      >
+        <div>
+          <img
+            src={Poster.match(/^http|^www/) ? Poster : Placeholder}
+            alt={`${Title} movie poster`}
+          />
+          <h4 className={styles.movieTitle}>
+            {Title}
+            {' '}
+            {Year}
+          </h4>
+        </div>
 
-          <div className={styles.movieCardButtons}>
+        <div className={styles.movieCardButtons}>
+          <button
+            className={styles.infoButton}
+            onClick={openInfo}
+          >
+            i
+          </button>
+
+          {category === 'search' && (
             <button
-              className={styles.infoButton}
-              onClick={this.showInfo}
+              value={movie}
+              onClick={() => nominate(movie)}
+              disabled={disableNominate}
             >
-              i
-            </button>
-
-            {this.props.category === 'search' && (
-              <button
-                value={this.props.movie}
-                onClick={() => this.props.nominate(this.props.movie)}
-                disabled={this.props.disableNominate}
-              >
               Nominate
-              </button>
-            )}
-            {this.props.category === 'nominations' && (
-              <button
-                className="removeButton"
-                value={this.props.movie}
-                onClick={() => this.props.remove(this.props.movie)}
-              >
-                <span className="fas fa-times" />
-              </button>
-            )}
-          </div>
-        </article>
+            </button>
+          )}
+          {category === 'nominations' && (
+            <button
+              className="removeButton"
+              value={movie}
+              onClick={() => remove(movie)}
+            >
+              <span className="fas fa-times" />
+            </button>
+          )}
+        </div>
+      </article>
 
-        {this.state.viewingInfo &&
-                    (
-                      <MovieInfo
-                        imdbID={imdbID}
-                        Year={Year}
-                        showInfo={this.showInfo}
-                      />
-                    )}
-      </li>
-    );
-  }
-}
+      {showInfo &&
+        <MovieInfo
+          imdbID={imdbID}
+          Year={Year}
+          showInfo={openInfo}
+        />
+      }
+    </li>
+  );
+};
 
 export default MovieCard;
